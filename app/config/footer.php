60e7ac52f7124256e15bbe5596b51c2d3c9a14b0
root <marcelohsilva36@gmail.com>
2022-05-27 22:11:50 -0300

    Site Fatido com sucesso!

---
+++ app/config/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; <?= date("Y");?> <a href="https://futebolpaulista.com.br">Abacate</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 5.3.0
+    </div>
+  </footer>
